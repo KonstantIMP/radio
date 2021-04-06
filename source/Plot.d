26615f9489952f0b82a631e6539dafa2d53d094d
KonstantIMP <mihedovkos@gmail.com>
2021-04-06 23:15:56 +0300

    Add plot class * Draw background and axes * Draw ys array

--- source/Plot.d
+++ source/Plot.d
@@ -7,6 +7,9 @@ import gtk.ScrolledWindow, gtk.DrawingArea, gtk.Label, gtk.Overlay, gtk.Widget;
 // Import Cairo for plot drawing
 import cairo.Context;
 
+// Framerate const
+immutable uint FRAMERATE = 11025;
+
 // @brief Widget for plot drawing
 // It is a composite widget with the struct :
 // GtkOverlay (Parent)
@@ -47,9 +50,13 @@ class Plot : Overlay {
 
     // @brief Create an points array to display
     // @return byte [] Points array to display
-    private byte [] evaluate () {
+    private float [] createYS () {
         // Empty for basic (parent) class
-        byte [] ys = new byte[0];
+        float [] ys = new float[FRAMERATE * 2];
+
+        // Zero ys
+        for (int i = 0; i < ys.length; i++) ys[i] = 1;
+
         return ys;
     }
 
@@ -58,8 +65,8 @@ class Plot : Overlay {
     // @return GtkAllocation with new widget size
     private GtkAllocation allocateSize () {
         GtkAllocation allocated_size;
-        allocated_size.height = allocated_size.width = -1;
-        allocated_size.x = allocated_size.y = 0;
+        allocated_size.height = allocated_size.width = 30;
+        allocated_size.x = allocated_size.y = 30;
         return allocated_size;
     }
 
@@ -77,6 +84,11 @@ class Plot : Overlay {
         plot_draw.queueDraw();
     }
 
+    // @brief onPlotAreaDraw Plot drawer
+    // This slot is called every time plot redraw
+    // @param[in]  cairo_context Cairo context for actually draw
+    // @param[in]  draw_area     Widget that contains cairo surface for drawing
+    // @return     bool          True if drawing was succesfull (Every time)
     private final bool onPlotAreaDraw (Scoped!Context cairo_context, Widget draw_area) {
         // Draw background (White)
         cairo_context.setSourceRgba(1.0, 1.0, 1.0, 1.0);
@@ -84,6 +96,61 @@ class Plot : Overlay {
 
         // Get drawing area size
         GtkAllocation w_alloc; draw_area.getAllocation(w_alloc);
+
+        // Create ys array
+        float [] ys = this.createYS();
+
+        // Prepare for axes drawing
+        cairo_context.setSourceRgba(0.0, 0.0, 0.0, 1.0);
+        cairo_context.setLineWidth(2);
+
+        // Calculate zero Y point and amplitude (because ys must contain values from -1 to 1)
+        float y_zero = w_alloc.height - 20; // Default value (for ys without < 0 values)
+        float ampl = w_alloc.height / 6 * 4; // Default value
+        for (int i = 0; i < ys.length; i = i + (FRAMERATE / 2)) {
+            if (ys[i] < 0) {
+                // The Y-center of plot
+                y_zero = w_alloc.height / 2;
+                // Increase ampl value
+                ampl = ampl / 2;
+                break;
+            }
+        }
+
+
+        // Draw Y axix
+        cairo_context.moveTo(20, w_alloc.height - 10);
+        cairo_context.lineTo(20, 10);
+        cairo_context.relLineTo(+2, +5);
+        cairo_context.relLineTo(-4, 0);
+        cairo_context.relLineTo(+2, -5);
+        cairo_context.stroke();
+
+        // Draw X axis
+        cairo_context.moveTo(10, y_zero);
+        cairo_context.relLineTo(w_alloc.width - 20, 0);
+        cairo_context.relLineTo(-5, +2);
+        cairo_context.relLineTo(0, -4);
+        cairo_context.relLineTo(+5, +2);
+        cairo_context.stroke();
+
+        // Set plot line params
+        cairo_context.setSourceRgba(0.0, 1.0, 0.0, 1.0);
+        cairo_context.setLineWidth(1);
+
+        // Actually plot line
+        // Step calculate
+        float step = cast(double)(w_alloc.width - 50) / ys.length;
+        // Start point
+        float current_point = 20;
+        // Move cursor at (0; 0)
+        cairo_context.moveTo(20, y_zero);
+
+        // Draw it
+        for (int i = 0; i < ys.length; i++) {
+            cairo_context.lineTo(current_point, y_zero - (ys[i] * ampl));
+            current_point = current_point + step;
+        } cairo_context.stroke();
 
         return true;
     }
